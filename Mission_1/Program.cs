8f7405be5f260d17cbb71446900aec308c49d6ab
Tamirlan <tuarshintamir@gmail.com>
2022-05-06 00:03:23 +0500

    Create mission 1 and GitIgnore

---
+++ Mission_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Write("Введите числа через пробел: ");
+
+string lineNum = Console.ReadLine();
+string[] arr = lineNum.Split();
+int[] mass = new int [arr.Length];
+int sum = 0;
+{
+for (int i = 0; i < mass.Length; i++)
+    mass[i] = Convert.ToInt32(arr[i]);
+for (int j = 0; j < mass.Length; j++)
+
+    if (mass[j] > 0)
+    sum++;    
+}   
+Console.WriteLine($" Количество чисел > 0: {sum}");
