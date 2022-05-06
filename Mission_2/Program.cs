b592d10d233fcae5d71681a202021212257f59a6
Tamirlan <tuarshintamir@gmail.com>
2022-05-06 11:50:40 +0500

    Точки пресечения X и Y

---
+++ Mission_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите k1 = ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите k2 = ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите b1 = ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите b2 = ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+
+double x = (b2 - b1)/(k1-k2);
+double y = k1 * x + b1;
+Console.WriteLine();
+Console.WriteLine($"Точка пересечения: x={x}, y={y}");
